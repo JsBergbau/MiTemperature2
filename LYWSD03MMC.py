0693d379613c757abc75da5e71c59296cfdd2e3b
JsBergbau <37013344+JsBergbau@users.noreply.github.com>
2020-01-06 23:09:18 +0000

    Addes calibration function

--- LYWSD03MMC.py
+++ LYWSD03MMC.py
@@ -20,9 +20,33 @@ class MyDelegate(btle.DefaultDelegate):
 			humidity=int.from_bytes(data[2:3],byteorder='little')
 			print("Temperature: " + str(temp))
 			print("Humidity: " + str(humidity))
+
+			if args.offset:
+				humidityCalibrated = humidity + args.offset
+				print("Calibrated humidity: " + str(humidityCalibrated))
+
+			if args.TwoPointCalibration:
+				offset1=args.offset1
+				offset2=args.offset2
+				p1y=args.calpoint1
+				p2y=args.calpoint2
+				p1x=p1y - offset1
+				p2x=p2y - offset2
+				m = (p1y - p2y) * 1.0 / (p1x - p2x) # y=mx+b
+				#b = (p1x * p2y - p2x * p1y) * 1.0 / (p1y - p2y)
+				b = p2y - m * p2x #would be more efficient to do this calculations only once
+				humidityCalibrated=m*humidity + b
+				if (humidityCalibrated > 100 ): #with correct calibration this should not happen
+					humidityCalibrated = 100
+				elif (humidityCalibrated < 0):
+					humidityCalibrated = 0
+				humidityCalibrated=round(humidityCalibrated,0)
+				print("Calibrated humidity: " + str(humidityCalibrated))
+
 			#print(data)
-		except:
+		except Exception as e:
 			print("Fehler")
+			print(e)
 		
 
 
@@ -41,6 +65,26 @@ parser=argparse.ArgumentParser()
 parser.add_argument("--device","-d", help="Set the device MAC-Adress in format AA:BB:CC:DD:EE:FF")
 parser.add_argument("--battery","-b", help="Read batterylevel every x update", metavar='N', type=int)
 parser.add_argument("--round","-r", help="Round temperature to one decimal place",action='store_true')
+
+# parentgroup=parser.add_argument_group("Calibration")
+# parentgroup.add_mutually_exclusive_group()
+
+#calgroup = parser.add_mutually_exclusive_group()
+offsetgroup = parser.add_argument_group("Offset calibration mode")
+offsetgroup.add_argument("--offset","-o", help="Enter an offset to the humidity value read",type=int)
+# calgroup.add_argument_group(offsetgroup)
+# parentgroup.add_argument_group(offsetgroup)
+
+complexCalibrationGroup=parser.add_argument_group("2 Point Calibration")
+complexCalibrationGroup.add_argument("--TwoPointCalibration","-2p", help="Use complex calibration mode. All arguments below are required",action='store_true')
+complexCalibrationGroup.add_argument("--calpoint1","-p1", help="Enter the first calibration point",type=int)
+complexCalibrationGroup.add_argument("--offset1","-o1", help="Enter the offset for the first calibration point",type=int)
+complexCalibrationGroup.add_argument("--calpoint2","-p2", help="Enter the second calibration point",type=int)
+complexCalibrationGroup.add_argument("--offset2","-o2", help="Enter the offset for the second calibration point",type=int)
+#
+# calgroup.add_argument_group(complexCalibrationGroup)
+# parentgroup.add_argument_group(complexCalibrationGroup)
+
 args=parser.parse_args()
 if args.device:
 	if re.match("[0-9a-fA-F]{2}([:]?)[0-9a-fA-F]{2}(\\1[0-9a-fA-F]{2}){4}$",args.device):
@@ -49,8 +93,17 @@ if args.device:
 		print("Please specify device MAC-Adress in format AA:BB:CC:DD:EE:FF")
 		os._exit(1)
 else:
-	print("Please specify device address")
+	parser.print_help()
+	# print("Please specify device address")
 	os._exit(1)
+if args.TwoPointCalibration:
+	if(not(args.calpoint1 and args.offset1 and args.calpoint2 and args.offset2)):
+		print("In 2 Point calibration you have to enter 4 points")
+		os._exit(1)
+	elif(args.offset):
+		print("Offset calibration and 2 Point calibration can't be used together")
+		os._exit(1)
+
 p=btle.Peripheral()
 cnt=0
 while True:
