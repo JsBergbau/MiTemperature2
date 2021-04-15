445a3f3e9628571b5cdf9f8c1ac7580aaefadc4b
JsBergbau <37013344+JsBergbau@users.noreply.github.com>
2021-04-15 13:02:01 +0200

    Bugfix
    No data received without using devicelist

--- LYWSD03MMC.py
+++ LYWSD03MMC.py
@@ -452,7 +452,7 @@ elif args.atc:
 			offset = paketStart + len(preeamble)
 				#print("reveived BLE packet")+
 			atcData_str = data_str[offset:offset+26]
-			ATCPaketMAC = atcData_str[0:6].upper()
+			ATCPaketMAC = atcData_str[0:12].upper()
 			macStr = mac.replace(":","").upper() 
 			atcIdentifier = data_str[(offset-4):offset].upper()
 
